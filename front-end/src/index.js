090b3ec99119d0877d628dbbd1eb91c9af44306b
Daniel Martin <danielmartin1205@gmail.com>
2019-12-30 22:39:17 -0500

    added dependencies, started basic router and provider setup for redux/react-router

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
+import thunk from "redux-thunk";
+import logger from 'redux-logger';
+import { createStore, Provider } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//create store,create reducers, add store to provider
+
+ReactDOM.render(
+    <Provider >
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
